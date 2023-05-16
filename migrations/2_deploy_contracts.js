258035557bcd86675da2d78526413ca0e019caf9
Kaustav Kamakhi Patro <kaustav.k48@gmail.com>
2023-05-17 00:59:13 +0530

    Update and rename 2_deploy_contract.js to 2_deploy_contracts.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+const Voting = artifacts.require("Voting");//importing the contract
+
+module.exports = function(deployer) {//deploying the contract
+  const candidates = [
+    web3.utils.utf8ToHex("Candidate 1"),
+    web3.utils.utf8ToHex("Candidate 2"),
+    web3.utils.utf8ToHex("Candidate 3")
+  ];
+  deployer.deploy(Voting, candidates, { from: accounts[0] });
+};
